b069c5d235a96b97cfb40c07217348f81c4d8021
adarsh <adarshpawse27@gmail.com>
2023-05-19 12:52:51 +0530

    just_fo_try

---
+++ poem.py
@@ -0,0 +1,28 @@
+print ('''Twinkle, twinkle, little star,
+How I wonder what you are !
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the trav’ller in the dark,
+Thanks you for your tiny spark,
+He could not see which way to go,
+If you did not twinkle so.
+
+In the dark blue sky you keep,
+And often thro' my curtains peep,
+For you never shut your eye,
+Till the sun is in the sky.
+
+'Tis your bright and tiny spark,
+Lights the trav’ller in the dark,
+Tho' I know not what you are,
+Twinkle, twinkle, little star.''')
+
+
+
+'''Taylor, "The Star".[1] The poem, which is in couplet form, was first published in 1806 in Rhymes for the Nursery, a collection of poems by Taylor and her sister Ann. It is sung to the tune of the French melody "Ah! vous dirai-je, maman", which was published in 1761 and later arranged by several composers, including Mozart with Twelve Variations on "Ah vous dirai-je, Maman".[2] The English lyrics have five stanzas, although only the first is widely known. It has a Roud Folk Song Index number '''
